126404edb83c4335797825c4f909698d3e9bd27e
Zhao Owen <kikakkz@hotmail.com>
2018-07-11 00:07:32 +0800

    Remove old compile script

--- project/source.mk
+++
@@ -1,6 +0,0 @@
-COMMON_INCLUDE_DIRS += $(rootdir)/$(MODULE)/include/onjson             \
-                       $(incdir)/cJSON $(incdir)/libonplatform
-
-COMMON_SRC_FILES := $(rootdir)/$(MODULE)/src/json/onjson.c
-
-COMMON_INST_HEADER_DIRS += $(rootdir)/$(MODULE)/include
